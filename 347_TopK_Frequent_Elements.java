08ede94c946418cf2e126c992c1d434360f74e68
bharath.manthati <bharath.manthati@servicenow.com>
2022-04-11 17:32:13 +0530

    347 - Top K Frequent Elements.

---
+++ 347_TopK_Frequent_Elements.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int el: nums)
+            map.put(el, map.getOrDefault(el,0)+1);
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a,b) -> a[1] - b[1]);
+        
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            pq.offer(new int[]{entry.getKey(), entry.getValue()});
+            if(pq.size() > k)
+                pq.poll();
+        }
+        
+        int[] ans = new int[k];
+        
+        while(k > 0) {
+            ans[k-1] = pq.poll()[0];
+            k--;
+        }
+        return ans;
+    }
+}
